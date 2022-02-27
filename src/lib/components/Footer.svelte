97e55b9238c43af8796cf064b0fffc316c9724b0
steven4714 <s.killian@ellusion.de>
2022-02-27 18:02:09 +0100

    change twitter script implementation to work on safari

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,13 @@
+<script>
+</script>
+
+<footer class="container">
+	
+</footer>
+
+<style lang="scss">
+	footer {
+		background-color: var(--color-secondary);
+		width: 100%;
+	}
+</style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,27 +1,6 @@
 <script>
 	import Icons from '$lib/components/Icons.svelte';
 	import Nav from '$lib/components/Nav.svelte';
-	import { onMount } from 'svelte';
-
-	onMount(async () => {
-		window.twttr = (function (d, s, id) {
-			var js,
-				fjs = d.getElementsByTagName(s)[0],
-				t = window.twttr || {};
-			if (d.getElementById(id)) return t;
-			js = d.createElement(s);
-			js.id = id;
-			js.src = 'https://platform.twitter.com/widgets.js';
-			fjs.parentNode.insertBefore(js, fjs);
-
-			t._e = [];
-			t.ready = function (f) {
-				t._e.push(f);
-			};
-
-			return t;
-		})(document, 'script', 'twitter-wjs');
-	});
 </script>
 
 <Icons />
